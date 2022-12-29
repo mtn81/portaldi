fafac0f78066817af4e1ad01ded8faf664d45ff5
mtn81 <nantsuka2011@gmail.com>
2022-12-29 18:45:36 +0900

    refactor

--- core/src/traits.rs
+++ core/src/traits.rs
@@ -62,73 +62,79 @@ mod tests {
 
     trait FooI {}
 
-    // sync component setup
-    struct Hoge;
-    impl DIPortal for Hoge {
-        type Output = Hoge;
-        fn create_for_di(_container: &DIContainer) -> Hoge {
-            Hoge {}
+    mod sync_test {
+        use super::*;
+
+        struct Hoge;
+        impl DIPortal for Hoge {
+            type Output = Hoge;
+            fn create_for_di(_container: &DIContainer) -> Hoge {
+                Hoge {}
+            }
+        }
+        impl FooI for Hoge {}
+
+        struct FooIProvider;
+        impl DIProvider for FooIProvider {
+            type Output = dyn FooI;
+            fn di_on(container: &DIContainer) -> DI<Self::Output> {
+                Hoge::di_on(container)
+            }
+        }
+
+        #[test]
+        #[allow(non_snake_case)]
+        fn test_same_instance_for_DIPortal() {
+            let hoge1 = Hoge::di().as_ref() as *const _;
+            let hoge2 = Hoge::di().as_ref() as *const _;
+            assert!(std::ptr::eq(hoge1, hoge2));
+        }
+
+        #[test]
+        #[allow(non_snake_case)]
+        fn test_same_instance_for_DIProvider() {
+            let foo1 = FooIProvider::di().as_ref() as *const _;
+            let foo2 = FooIProvider::di().as_ref() as *const _;
+            assert!(std::ptr::eq(foo1, foo2));
         }
     }
-    impl FooI for Hoge {}
 
-    struct FooIProvider;
-    impl DIProvider for FooIProvider {
-        type Output = dyn FooI;
-        fn di_on(container: &DIContainer) -> DI<Self::Output> {
-            Hoge::di_on(container)
+    mod async_test {
+        use super::*;
+
+        struct AsyncHoge;
+        #[async_trait]
+        impl AsyncDIPortal for AsyncHoge {
+            type Output = AsyncHoge;
+            async fn create_for_di(_container: &DIContainer) -> AsyncHoge {
+                AsyncHoge {}
+            }
         }
-    }
+        impl FooI for AsyncHoge {}
 
-    // async component setup
-    struct AsyncHoge;
-    #[async_trait]
-    impl AsyncDIPortal for AsyncHoge {
-        type Output = AsyncHoge;
-        async fn create_for_di(_container: &DIContainer) -> AsyncHoge {
-            AsyncHoge {}
+        struct AsyncFooIProvider;
+        #[async_trait]
+        impl AsyncDIProvider for AsyncFooIProvider {
+            type Output = dyn FooI;
+            async fn di_on(container: &DIContainer) -> DI<Self::Output> {
+                AsyncHoge::di_on(container).await
+            }
         }
-    }
-    impl FooI for AsyncHoge {}
 
-    struct AsyncFooIProvider;
-    #[async_trait]
-    impl AsyncDIProvider for AsyncFooIProvider {
-        type Output = dyn FooI;
-        async fn di_on(container: &DIContainer) -> DI<Self::Output> {
-            AsyncHoge::di_on(container).await
+        #[tokio::test]
+        #[allow(non_snake_case)]
+        async fn test_same_instance_for_AsyncDIPortal() {
+            let hoge1 = AsyncHoge::di().await.as_ref() as *const _;
+            let hoge2 = AsyncHoge::di().await.as_ref() as *const _;
+            assert!(std::ptr::eq(hoge1, hoge2));
         }
-    }
 
-    #[test]
-    #[allow(non_snake_case)]
-    fn test_same_instance_for_DIPortal() {
-        let hoge1 = Hoge::di().as_ref() as *const _;
-        let hoge2 = Hoge::di().as_ref() as *const _;
-        assert!(std::ptr::eq(hoge1, hoge2));
-    }
-
-    #[test]
-    #[allow(non_snake_case)]
-    fn test_same_instance_for_DIProvider() {
-        let foo1 = FooIProvider::di().as_ref() as *const _;
-        let foo2 = FooIProvider::di().as_ref() as *const _;
-        assert!(std::ptr::eq(foo1, foo2));
-    }
-
-    #[tokio::test]
-    #[allow(non_snake_case)]
-    async fn test_same_instance_for_AsyncDIPortal() {
-        let hoge1 = AsyncHoge::di().await.as_ref() as *const _;
-        let hoge2 = AsyncHoge::di().await.as_ref() as *const _;
-        assert!(std::ptr::eq(hoge1, hoge2));
-    }
-
-    #[tokio::test]
-    #[allow(non_snake_case)]
-    async fn test_same_instance_for_AsyncDIProvider() {
-        let foo1 = AsyncFooIProvider::di().await.as_ref() as *const _;
-        let foo2 = AsyncFooIProvider::di().await.as_ref() as *const _;
-        assert!(std::ptr::eq(foo1, foo2));
+        #[tokio::test]
+        #[allow(non_snake_case)]
+        async fn test_same_instance_for_AsyncDIProvider() {
+            let foo1 = AsyncFooIProvider::di().await.as_ref() as *const _;
+            let foo2 = AsyncFooIProvider::di().await.as_ref() as *const _;
+            assert!(std::ptr::eq(foo1, foo2));
+        }
     }
 }
